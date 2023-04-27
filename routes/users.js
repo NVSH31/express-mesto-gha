2db0b212bdbd0a3144146f0d672c5e7be42e9f08
Natasha Shpak <n.v.shpak@yandex.ru>
2023-04-27 22:19:05 +0400

    ref validators.users

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,10 @@ const { celebrate, Joi } = require('celebrate');
 
 const router = require('express').Router();
 
-const { url } = require('../utils/const');
+// const { url } = require('../utils/const');
+const {
+  validateId, validateUserNameAbout, validateUrlAvatar,
+} = require('../validators/validators');
 const {
   getUsers, getUser, updateProfile, updateAvatar, getMe,
 } = require('../controllers/users');
@@ -12,18 +15,22 @@ router.get('/', auth, getUsers);
 router.get('/me', auth, getMe);
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    // userId: Joi.string().alphanum().length(24),
+    userId: validateId,
   }),
 }), auth, getUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    // name: Joi.string().min(2).max(30),
+    // about: Joi.string().min(2).max(30),
+    name: validateUserNameAbout,
+    about: validateUserNameAbout,
   }),
 }), auth, updateProfile);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().regex(url),
+    // avatar: Joi.string().required().regex(url),
+    avatar: validateUrlAvatar,
   }),
 }), auth, updateAvatar);
 
---
+++ validators/validators.js
@@ -0,0 +1,20 @@
+const { Joi } = require('celebrate');
+const { url } = require('../utils/const');
+
+const validateUserNameAbout = Joi.string().min(2).max(30);
+const validateCardName = Joi.string().required().min(2).max(30);
+const validateCardLink = Joi.string().required().regex(url).min(2);
+const validateUrlAvatar = Joi.string().required().regex(url).min(2);
+const validateId = Joi.string().alphanum().length(24);
+const validateEmail = Joi.string().required().email();
+const validatePassword = Joi.string().required().min(6);
+
+module.exports = {
+  validateUserNameAbout,
+  validateCardName,
+  validateCardLink,
+  validateUrlAvatar,
+  validateId,
+  validateEmail,
+  validatePassword,
+};
